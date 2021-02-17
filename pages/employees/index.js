cd0b54fc797a66972f11c9f0b85076d042d1a959
Anupam Dahal <anupamdahal1@gmail.com>
2021-02-16 22:29:14 -0600

    Added employees card

--- pages/employees/index.js
+++ pages/employees/index.js
@@ -1,16 +1,49 @@
 import { Toolbar } from "../../components/Toolbar"
-import styles from "../../styles/Home.module.css"
+import styles from "../../styles/Employee.module.css"
+import employeesRecord from '../../public/database/employees.json'
+import { useRouter } from "next/router"
 
-export const employees = () => {
+export const employees = ({employeeList}) => {
+
+  const router = useRouter()
+
   return(
     <div className='page-container'>
       <Toolbar />
       <div className={styles.main}>
-        <h1> Nothing Here!</h1>
-        <h3> Please go to specific employee directory to see the classified employee data</h3>
+        {
+          employeeList.map((name, index) => {
+            return(
+              <div
+                className={styles.employeeNameCard}
+                key={index}
+                onClick={() => {
+                  router.push(`/employees/${name}`)
+                }}
+              >
+                <div
+                  className={styles.employeeName}
+                >
+                  {name}
+                </div>
+              </div>
+            )
+          })
+        }
       </div>
     </div>
   )
+}
+
+export const getStaticProps =  async () => {
+
+  const employeeList = Object.keys(employeesRecord);
+
+  return {
+    props: {
+      employeeList
+    }
+  }
 }
 
 export default employees
